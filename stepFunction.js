d9fa6c8f3e7aed242aa6443ef82ecf32ceb4e7f6
RizwanMalvat260 <56027557+RizwanMalvat260@users.noreply.github.com>
2019-10-09 17:48:50 +0530

    Update stepFunction.js

--- stepFunction.js
+++ stepFunction.js
@@ -346,24 +346,25 @@ var stepFunction = {
 var getLambdaMappings = async function () {
   return new Promise(resolve => {
     fs.readFile('../../package.json', 'utf8', function (err, contents) {
-      allMappingFiles = Object.keys(JSON.parse(contents).dependencies);
-      var promises = []
-      allMappingFiles.forEach((listItem) => {
-        const path = '/usr/src/node-red/node_modules/' + listItem.trim() + '/mapping.json';
-        // const tempPath = '../nodered-docker/mapping.json';
-        
-        promises.push(fileReader(path));
-      })
-      var finalObject = null;
-      when.all(promises).then((data) => {
-        finalObject =  Object.assign({}, ...data); 
-        resolve(finalObject)
-      })
+      if (contents === 'undefined' || contents === undefined) {
+        allMappingFiles = Object.keys(JSON.parse(contents).dependencies);
+        var promises = []
+        allMappingFiles.forEach((listItem) => {
+          const path = '/usr/src/node-red/node_modules/' + listItem.trim() + '/mapping.json';
+          // const tempPath = '../nodered-docker/mapping.json';
+
+          promises.push(fileReader(path));
+        })
+        var finalObject = null;
+        when.all(promises).then((data) => {
+          finalObject = Object.assign({}, ...data);
+          resolve(finalObject)
+        })
+      }
     });
   });
 
 }
-
 function fileReader(tempPath) {
   return new Promise(resolve => {
     fs.readFile(tempPath, 'utf8', function (err, contents) {
